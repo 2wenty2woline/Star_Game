5ebaf6819ed5690938f71639b1d37ad137822234
TarasDoletsky <4983879@gmail.com>
2021-10-20 15:34:29 +0300

    commit

--- Star_Game/main.cpp
+++ Star_Game/main.cpp
@@ -2,29 +2,9 @@
 
 int main()
 {
-<<<<<<< Updated upstream
-	// Объект, который, собственно, является главным окном приложения
-	RenderWindow window(VideoMode(200, 200), "SFML Works!");
-=======
-										// --
 	Game game;
->>>>>>> Stashed changes
 
-	// Главный цикл приложения. Выполняется, пока открыто окно
-	while (window.isOpen())
-	{
-		// Обрабатываем очередь событий в цикле
-		Event event;
-		while (window.pollEvent(event))
-		{
-			// Пользователь нажал на «крестик» и хочет закрыть окно?
-			if (event.type == Event::Closed)
-				// тогда закрываем его
-				window.close();
-		}
-		// Отрисовка окна	
-		window.display();
-	}
+	game.run();
 
 	return 0;
 }
